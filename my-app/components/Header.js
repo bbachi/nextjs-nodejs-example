5d55e0e9d34badfa050f3c2af5394f7a204df979
bbachi <bhargavbachina@yahoo.com>
2020-11-26 15:58:49 -0600

    initial commit

---
+++ my-app/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Header = () => {
+    return(
+        <div className="header">
+            <h1>Next.js With NodeJS</h1>
+        </div>
+    )
+}
---
+++ my-app/pages/index.js
@@ -0,0 +1,9 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+import App from '../components/App'
+
+export default function Home() {
+  return (
+    <App />
+  )
+}
